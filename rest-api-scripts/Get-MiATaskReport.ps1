3b04e052dc9d2fc935f33dbd690e3e95ad88cd9b
Tony Perri <tony.perri@progress.com>
2023-10-04 16:30:04 -0400

    Support Advanced Tasks

--- rest-api-scripts/Get-MiATaskReport.ps1
+++ rest-api-scripts/Get-MiATaskReport.ps1
@@ -29,8 +29,11 @@ try { Connect-MIAServer -Hostname $hostname -Credential $Credential } catch { th
 $hostList = Get-MIAHost
 $taskList = Get-MIATask
 
+# Initialize an array to hold the results for exporting to csv
+$taskReport = @()
+
 # Loop through each task
-$taskReport = $taskList | ForEach-Object {
+$taskList | ForEach-Object {
     # Get full details for task
     $task = $_ | Get-MIATask
 
@@ -38,15 +41,41 @@ $taskReport = $taskList | ForEach-Object {
     $source = $task.steps.Source | Select-Object -First 1
 
     # Only report on the first destination
-    $dest = $task.steps.Destination | Select-Object -First 1
+    $dest = switch ($task.TT) {
+        'COS'   { $task.steps.For.steps.Destination | Select-Object -First 1 }
+        default { $task.steps.Destination | Select-Object -First 1 }
+    }
 
     # Only report on the first success next action
-    $successNA = $task.NextActions.NextAction | Where-Object { $_.DoIfSuccess -eq 1 } | Select-Object -First 1
+    $successNA = switch ($task.TT) {
+        'COS'   {
+            ($task.steps.If.When | Where-Object { 
+                $_.Criteria.comp.a -eq '[TASKSTATUS]' -and 
+                $_.Criteria.comp.b -eq 'Success' -and 
+                $_.steps.Email 
+            } | Select-Object -First 1).steps.Email
+        }
+        default {
+            $task.NextActions.NextAction | Where-Object { $_.DoIfSuccess -eq 1 } | Select-Object -First 1
+        }
+    }
 
     # Only report on the first failure next action
-    $failureNA = $task.NextActions.NextAction | Where-Object { $_.DoIfFailure -eq 1 } | Select-Object -First 1
+    $failureNA = switch ($task.TT) {
+        'COS'   {
+            ($task.steps.If.When | Where-Object { 
+                $_.Criteria.comp.a -eq '[TASKSTATUS]' -and 
+                $_.Criteria.comp.b -eq 'Failure' -and 
+                $_.steps.Email 
+            } | Select-Object -First 1).steps.Email
+        }
+        default {
+            $task.NextActions.NextAction | Where-Object { $_.DoIfFailure -eq 1 } | Select-Object -First 1
+        }
+    }
 
-    [pscustomobject]@{
+    # Append the info for this task to an array
+    $taskReport += [pscustomobject]@{
         Name = $task.Name
         SourceHost = ($hostList | Where-Object { $_.ID -eq $source.HostID}).Name
         SourcePath = if ($source.Type -eq 'siLock') {$source.FolderName} else {$source.Path}
@@ -55,12 +84,13 @@ $taskReport = $taskList | ForEach-Object {
         SuccessEmail = $successNA.AddressTo
         FailureEmail = $failureNA.AddressTo
     }
-}
+
+    # Write the item to the pipeline too so it is displayed to the console
+    $taskReport[-1]
+
+} | Format-Table
+
+Disconnect-MIAServer
 
 # Write out the results to a CSV File
 $taskReport | Export-Csv -Path ("{0}_{1:yyyy-MM-ddThhmmss}_TaskReport.csv" -f $Hostname, (Get-Date))
-
-# Write out the results to the console
-$taskReport | Format-Table
-
-Disconnect-MIAServer
