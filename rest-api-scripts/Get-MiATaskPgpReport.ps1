72740032e0373dabbed75224f8ccc81fcef2741c
Tony Perri <tony.perri@progress.com>
2023-11-08 11:29:45 -0500

    Updated to use v0.3.4 module

--- rest-api-scripts/Get-MiATaskPgpReport.ps1
+++ rest-api-scripts/Get-MiATaskPgpReport.ps1
@@ -1,4 +1,4 @@
-#Requires -Modules MOVEit.MIA
+#Requires -Modules @{ ModuleName="MOVEit.MIA"; ModuleVersion="0.3.4" }
 #Requires -Version 7
 
 <#
@@ -28,43 +28,8 @@ param (
 # Exit if we can't connect
 try { Connect-MIAServer -Hostname $hostname -Credential $Credential } catch { throw }
 
-# Define a function to recurse through a task and find all steps
-function Select-MiATaskStep {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory,ValueFromPipeline)]
-        [psobject]$InputObject,
-        
-        [Parameter()]
-        [ValidateSet('Source', 'Process', 'Destination', 'NextAction', 'Email')]
-        [string[]]$StepName = @('Source', 'Process', 'Destination', 'NextAction', 'Email')
-    )
-
-    begin {        
-        $containerName = 'steps', 'For', 'If', 'When'
-    }
-
-    process {        
-        $InputObject.PSObject.Properties | ForEach-Object {
-            if ($_.Name -in $StepName) {
-                # This is a property we are looking for
-                [pscustomobject]@{$_.Name = $_.Value}                
-            }
-            elseif ($_.Name -in $containerName) {
-                # Recurse
-                $_.Value | Select-MiATaskStep -StepName $StepName
-            }
-        }
-    }
-}
-
-$pgpScript = @(
-    [PSCustomObject]@{Id = 100021; Name = 'PGP Encrypt Only'}
-    [PSCustomObject]@{Id = 100022; Name = 'PGP Encrypt and Sign'}
-    [PSCustomObject]@{Id = 100023; Name = 'PGP Decrypt'}
-)
-
-# Get a list of keys and tasks
+# Get a list of pgp scripts, pgpkeys and tasks
+$pgpScriptList = Get-MIAStandardScript -Name 'PGP*' -Fields ID,Name
 $pgpKeyList = Get-MIAPgpKey
 $taskList = Get-MIATask
 
@@ -76,13 +41,13 @@ $taskList | ForEach-Object {
     $task = $_
 
     # Determine if this task has any process steps that do PGP and, if so, process this task
-    $task | Select-MiATaskStep -StepName Process | Where-Object {$pgpScript.Id -eq $_.Process.ScriptID} | ForEach-Object {
-        $process = $_.process
+    $task | Select-MiATaskStep -StepType Process -Expand | Where-Object {$_.ScriptID -in $pgpScriptList.Id} | ForEach-Object {
+        $scriptName = ($pgpScriptList | Where-Object Id -eq $_.ScriptID).Name
 
         # Determine the recipient key(s) used and their expiration date.  The key could be specified at the process or at
         # the task level.
         $recipientKey = $null
-        if ($parameter = ($process.Parameters.Parameter | Where-Object Name -eq 'PGPRecipientKey') ??
+        if ($parameter = ($_.Parameters.Parameter | Where-Object Name -eq 'PGPRecipientKey') ??
                             ($task.Parameters.Parameter | Where-Object Name -eq 'PGPRecipientKey')) {
             $recipientKeyId = $parameter.value -split '\|'
             $recipientKey = $pgpKeyList | Where-Object {$_.Id -in $recipientKeyId}
@@ -93,7 +58,7 @@ $taskList | ForEach-Object {
             Name                = $task.Name
             Type                = $task.TT
             Scheduled           = $task.Scheduled
-            Process             = ($pgpScript | Where-Object Id -eq $process.ScriptID).Name
+            Process             = $scriptName
             RecipientKey        = $recipientKey.Uid #-join '; '
             RecipientKeyPubPriv = $recipientKey.PubPriv #-join '; '
             RecipientKeyExpires = $recipientKey.Expires #-join '; '
@@ -107,6 +72,6 @@ $taskList | ForEach-Object {
 Disconnect-MIAServer
 
 # Write out the results to a CSV File
-if (!$SkipCsv) {
+if (-not $SkipCsv) {
     $taskReport | Export-Csv -Path ("{0}_{1:yyyy-MM-ddThhmmss}_PgpTaskReport.csv" -f $Hostname, (Get-Date))
 }
